915b4bbba9722cbafef6ce61f05297d4b69905af
KhanStan <soubankhan3@gmail.com>
2022-04-18 22:31:00 +0530

    Updated SW

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import * as serviceWorker from './serviceWorker.js';
+import * as serviceWorker from './serviceWorker';
 import App from './components/app.jsx';
 
 const rootElement = document.getElementById('root');
