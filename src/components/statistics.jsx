1bd153cb92db7a7a92c54ed17a1ca0cb445b4e5f
ziaraheem <90146416+ziaraheem@users.noreply.github.com>
2022-06-10 19:45:06 +0530

    added average

--- src/components/statistics.jsx
+++ src/components/statistics.jsx
@@ -1,27 +1,27 @@
-import { React, useState } from 'react';
-import { Typography } from '@mui/material';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, registerables } from 'chart.js';
+import { React, useState } from "react";
+import { Typography } from "@mui/material";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
 ChartJS.register(...registerables);
 
 export default function Statistics(props) {
-  const oldData = JSON.parse(localStorage.getItem('oldData'));
-  const oldGraphData = localStorage.getItem('oldGraphData')
-    ? JSON.parse(localStorage.getItem('oldGraphData'))
+  const oldData = JSON.parse(localStorage.getItem("oldData"));
+  const oldGraphData = localStorage.getItem("oldGraphData")
+    ? JSON.parse(localStorage.getItem("oldGraphData"))
     : [];
-  const oldGraphTimeData = localStorage.getItem('oldGraphTimeData')
-    ? JSON.parse(localStorage.getItem('oldGraphTimeData'))
+  const oldGraphTimeData = localStorage.getItem("oldGraphTimeData")
+    ? JSON.parse(localStorage.getItem("oldGraphTimeData"))
     : [];
 
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        position: "top",
       },
       title: {
         display: true,
-        text: 'Graph View of Ayyahs Per Session (Showing last 5 sessions)',
+        text: "Graph View of Ayyahs Per Session (Showing last 5 sessions)",
       },
     },
   };
@@ -35,13 +35,13 @@ export default function Statistics(props) {
         : oldGraphTimeData,
     datasets: [
       {
-        label: 'Checkpoint',
+        label: "Checkpoint",
         data:
           oldGraphData.length > 5
             ? oldGraphData.slice(oldGraphData.length - 5, oldGraphData.length)
             : oldGraphData,
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
     ],
   };
@@ -51,10 +51,15 @@ export default function Statistics(props) {
 
   const [totalAayahsRead] = useState(oldData ? oldData.totalAayahsRead : 0);
   const totalAayaths = 6236;
+  const avgFormula =
+    (oldGraphData.length > 5
+      ? oldGraphData.slice(oldGraphData.length - 5, oldGraphData.length)
+      : oldGraphData
+    ).reduce((partialSum, a) => partialSum + a, 0) / oldGraphData.length;
 
   return (
-    <div style={{ textAlign: '-webkit-center' }}>
-      <Typography style={{ margin: '15px' }}>
+    <div style={{ textAlign: "-webkit-center" }}>
+      <Typography style={{ margin: "15px" }}>
         <strong>Salam!</strong> Your progress so far:
       </Typography>
       <Line options={options} data={data} />
@@ -79,7 +84,7 @@ export default function Statistics(props) {
           <tr>
             <td>Quran Left</td>
             <td>
-              {totalAayaths - totalAayahsRead} Aayahs |{'  '}
+              {totalAayaths - totalAayahsRead} Aayahs |{"  "}
               {parseFloat(
                 ((totalAayaths - totalAayahsRead) / totalAayaths) * 100
               ).toFixed(2)}
@@ -91,6 +96,17 @@ export default function Statistics(props) {
             <td>Last Aayah</td>
             <td>
               {currentSurah} : {currentAayahNo}
+            </td>
+          </tr>
+          <tr>
+            <td>Average Aayah(s) read per session (Avg. of last 5 sessions)</td>
+            <td>{avgFormula.toFixed(2)} Aayah's</td>
+          </tr>
+          <tr>
+            <td>Sesssions to Complete (Based on Avg.)</td>
+
+            <td>
+              {((totalAayaths - totalAayahsRead) / avgFormula).toFixed(2)}{" "}
             </td>
           </tr>
         </tbody>
