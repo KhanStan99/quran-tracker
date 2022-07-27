83062d1f7c2511d9f13f579e9d31ce30d59b643a
Hidayat Khan <hidayat.khan@sleek.com>
2022-07-27 14:34:08 +0530

    Added Login/Signup components

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,30 +1,123 @@
-import React from 'react';
-import { Grid, TextField, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, TextField, Paper, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
+import './login.css';
 
 export default function Login() {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [isLoginOtpSend, setIsLoginOtpSend] = useState(false);
+
   return (
-    <div style={{ padding: 30 }}>
-      <Paper>
-        <Grid
-          container
-          spacing={3}
-          direction={'column'}
-          justify={'center'}
-          alignItems={'center'}
-        >
-          <Grid item xs={12}>
-            <TextField label="Username"></TextField>
+    <Grid container>
+      <Typography
+        variant="h5"
+        textAlign={'center'}
+        color={'#FFF'}
+        marginTop={'24px'}
+      >
+        Assalamualikum! Welcome to Quran Tracker App! Please login or signup and
+        start your Quran Journey!
+      </Typography>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={6}
+        spacing={2}
+        direction={'column'}
+        justify={'center'}
+        alignItems={'center'}
+        textAlign={'center'}
+      >
+        <Paper style={{ padding: '24px', margin: '24px' }}>
+          <Typography variant="h4">Login</Typography>
+          <Grid item style={{ margin: '12px' }}>
+            <TextField
+              disabled={isLoginOtpSend}
+              required
+              fullWidth
+              label="Email"
+              variant="filled"
+            />
           </Grid>
-          <Grid item xs={12}>
-            <TextField label="Password" type={'password'}></TextField>
+          <Grid item style={{ margin: '12px' }}>
+            <TextField
+              disabled={!isLoginOtpSend}
+              required
+              fullWidth
+              label="OTP"
+              variant="filled"
+            />
           </Grid>
 
           <Grid item xs={12}>
-            <Button variant="outlined">Login</Button>
+            <Button variant="contained" onClick={() => setIsLoginOtpSend(true)}>
+              {isLoginOtpSend ? 'Verify OTP' : 'Send OTP'}
+            </Button>
           </Grid>
-        </Grid>
-      </Paper>
-    </div>
+        </Paper>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={6}
+        spacing={2}
+        justify={'center'}
+        alignItems={'center'}
+        textAlign={'center'}
+      >
+        <Paper style={{ padding: '24px', margin: '24px' }}>
+          <Typography variant="h4">SignUp</Typography>
+          <Grid item style={{ margin: '12px' }}>
+            <TextField
+              fullWidth
+              required
+              label="Name"
+              variant="filled"
+            />
+          </Grid>
+          <Grid item style={{ margin: '12px' }}>
+            <TextField
+              required
+              fullWidth
+              label="Email"
+              variant="filled"
+            />
+          </Grid>
+          <Grid style={{ textAlign: 'start' }}>
+            <Typography variant="body1" textAlign={'center'}>
+              Remeber these points before continuing:
+            </Typography>
+            <ul>
+              <li>
+                <Typography variant="body1">
+                  ✅ We save your email address to sync your data to database,
+                  so you can use the app anywhere.
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body1">
+                  ✅ We will send you login OTP to your email anytime on you
+                  want to login.
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body1">
+                  ✅ We may send you emails later related to your account or app
+                  features later if required.
+                </Typography>
+              </li>
+            </ul>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button variant="contained">SignUp</Button>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
