87dd04da3f062065b48658d0c53215b60a90bc91
Hidayat Khan <hidayat.khan@sleek.com>
2023-01-11 16:18:22 +0530

    updated backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base_url: 'https://quran-read-tracker.herokuapp.com/',
+    base_url: 'https://web-production-1ec8.up.railway.app/',
     // base_url: 'http://localhost:8080/',
   };
   
