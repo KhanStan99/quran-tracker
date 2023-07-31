e4a16b95ea4db6f967b6f0151f89ecf19a586cc8
Hidayat Khan <hidayat.khan@sleek.com>
2023-07-31 21:42:20 +0530

    moved to vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base_url: 'https://quran-read-tracker.up.railway.app/',
+    base_url: 'https://quran-tracker-back.vercel.app/',
     // base_url: 'http://localhost:8080/',
   };
   
