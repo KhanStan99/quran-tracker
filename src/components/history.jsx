324407e0d3e5303948c55d2a2b482e331f9f6513
Hidayat Khan <soubankhan3@gmail.com>
2023-09-17 01:38:58 +0530

    Updated history section

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,19 +1,43 @@
-import { React, useState, useEffect, useContext } from 'react';
-import { Chart as ChartJS, registerables } from 'chart.js';
-import dataService, { getHistoryData } from '../services/data-service';
-
+import { React, useState, useEffect } from 'react';
+import { getHistoryData } from '../services/data-service';
+import moment from 'moment';
 
 export default function History(props) {
-  const [data, setDate] = useState(null);
+  const [data, setDate] = useState([]);
   useEffect(() => {
-    getHistoryData(JSON.parse(localStorage.getItem('user')).userId).then((data) => {
-      console.log("fddfsd", data);
-    })
+    getHistoryData(JSON.parse(localStorage.getItem('user')).userId).then(
+      (response) => {
+        if (response.data && response.data.length > 0) {
+          setDate(response.data);
+        }
+      }
+    );
   }, []);
 
   return (
     <div style={{ textAlign: '-webkit-center' }}>
-      You dont have any history added
+      {data.length > 0 ? (
+        <table className="progress">
+          <thead>
+            <tr>
+              <th>Started At</th>
+              <th>Finished At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{moment(item.startAt).format('DD/MM/YY - hh:mm a')}</td>
+                  <td>{moment(item.endAt).format('DD/MM/YY - hh:mm a')}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        'You dont have any history added'
+      )}
     </div>
   );
 }
